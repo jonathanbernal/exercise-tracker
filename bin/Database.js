8389e4701408f7b4259cdf48b82dda3c01de594b
Jonathan Bernal <jonathanbernal@users.noreply.github.com>
2023-04-14 23:24:53 -0400

    Make database await for asynchronous connection

--- bin/Database.js
+++ bin/Database.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const Database = async () => {
-    const connection = mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+    const connection = await mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
     return connection;
 }
 
-module.exports = Database().then(result => console.log(result))
+module.exports = Database().then(()=>console.log('Successfully connected to DB'))
     .catch(err => console.log(err));

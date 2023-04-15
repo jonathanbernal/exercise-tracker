2b99d044f5861d135e06d4f1435c0ffa59f781ff
Jonathan Bernal <jonathanbernal@users.noreply.github.com>
2023-04-15 13:52:39 -0400

    Add path, bodyParser, and implement exercise POST request

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path = require('path');
 require('dotenv').config();
 
 // Set up middleware
 app.use(cors());
-app.use(express.static('/public', dirname__ + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended: true}));
 
 // User-defined imports
 const database = require('./bin/database.js');
@@ -19,12 +22,46 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-app.post('/api/users', async (req, res) => {
+app.get('/api/users', async (req, res) => {
+  const allUserDocuments = await User.find();
+  res.json({users: allUserDocuments});
+});
 
+app.post('/api/users', (req, res) => {
+  const newUser = new User({
+    _id: new mongoose.Types.ObjectId(),
+    username: req.body.username
+  });
+
+  newUser.save().then(savedUser => res.json({user: savedUser}))
+    .catch(err => {
+      console.log('Error adding user. User already exists in DB');
+      res.json({error: 'user already exists'});
+    });
 });
 
 app.post('/api/users/:_id/exercises', async (req, res) => {
+  const requestedId = req.body[':_id'];
+  const userExists =  await User.findOne({_id: requestedId});
+  
+  if (userExists) {
+    const newExercise = new Exercise({
+      username: userExists._id,
+      description: req.body.description,
+      duration: req.body.duration,
+      date: req.body.date ? new Date(req.body.date).toDateString() : new Date().toDateString()
+    });
 
+    await newExercise.populate('username', 'username -_id');
+    console.log(newExercise);
+    await newExercise.save();
+    userExists.exercises.push(newExercise);
+    await userExists.populate('exercises', '-username');
+    const result = await userExists.save();
+    res.send(result);
+  } else {
+    res.json({error: `user with id ${req.body[':_id']} does not exist in DB`});
+  }
 });
 
 // TODO: think about how to implement get route for
